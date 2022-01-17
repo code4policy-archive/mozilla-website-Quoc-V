178a804807ea75f9e69f751cbe2674ec7322bad0
Quoc-V <viet.nguyen@rwth-aachen.de>
2022-01-16 19:41:15 -0500

    added picture changing and hiding feature

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,31 @@
 const myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello world!';
-document.querySelector('html').onclick = function() {
+
+document.querySelector('#myImage').onclick = function() {
     alert('Ouch! Stop poking me!');
 }
+
+let myImage = document.querySelector('img')
+let myButton = document.querySelector('button');
+
+function changeImage() {
+	let image = document.getElementById('myImage');
+	if (image.src.match("firefox")){
+		image.src = "Images/Dogecoin.jpg"
+	}
+	else {
+		image.src = "Images/firefox-icon.png"
+	}
+}
+
+function hideImage(){
+	let image = document.getElementById('myImage');
+	if (image.src.match("Images")){
+		image.src = ""
+	}
+	else {
+		image.src = "Images/firefox-icon.png"
+	}
+
+}
+
